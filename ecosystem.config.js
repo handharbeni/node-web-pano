4bdf708e102ee063c930ccb47767a06bffd92199
Muhammad Handharbeni <mhandharbeni@gmail.com>
2020-11-15 21:18:45 +0700

    first commit

---
+++ ecosystem.config.js
@@ -0,0 +1,44 @@
+module.exports = {
+    apps: [
+      {
+        name: " Web Virtual Room PRASMUL",
+        script: "index.js",
+  
+        // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+        args: "Prasmul Pano",
+        instances: "max",
+        exec_mode: "cluster",
+        autorestart: true,
+        watch: true,
+        kill_timeout: 3000,
+        listen_timeout: 3000,
+        ignore_watch: [
+          "upload",
+          "upload/files",
+          ".git",
+          "node_modules",
+          "tools/db/sqlite/ticketing.db",
+        ],
+        max_memory_restart: "8G",
+        env: {
+          NODE_ENV: "development",
+        },
+        env_production: {
+          NODE_ENV: "production",
+        },
+      },
+    ],
+  
+    deploy: {
+      production: {
+        user: "node",
+        host: "212.83.163.1",
+        ref: "origin/master",
+        repo: "git@github.com:repo.git",
+        path: "/var/www/production",
+        "post-deploy":
+          "npm install && pm2 reload ecosystem.config.js --env production",
+      },
+    },
+  };
+  
