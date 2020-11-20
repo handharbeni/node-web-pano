c016488c9dd77f5dd26c2940e3b5988ce6f46075
Muhammad Handharbeni <mhandharbeni@gmail.com>
2020-11-20 23:39:33 +0700

    update

--- index.js
+++ index.js
@@ -3,34 +3,40 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const compression = require('compression');
-const youtubedl = require('youtube-dl');
-// const redis = require("redis");
-const redis = require("redis-node");
-const client = redis.createClient();
-client.select(2);
+const Youtube = require('youtube-stream-url');
 
-const video = youtubedl('https://www.youtube.com/watch?v=vedBvGiXw2E', ['--format=22/17/18'], { cwd: __dirname });
+const redis = require("redis");
+const client = redis.createClient();
+const videoUrl = 'https://www.youtube.com/watch?v=ckOeCDI2ElU';
 
 app.use(express.json());
 app.use(express.static("express"));
 app.use(compression());
 
+const maxTtl = 60 * 60 * 3;
+
 app.use('/lobby', function(req,res){
   res.sendFile(path.join(__dirname+'/express/index-lobi.html'));
 });
 
-app.use('/videos', async function(req, res){
-  client.get('urlVideos', function(err, reply){
-    if(reply==null){
-      video.on('info', function (info) {
-        client.set('urlVideos', info.url);
-        client.expire('urlVideos', (60 * 60 * 6), () => {});
-        res.send(info.url);
-      });
+app.use('/videos-json', async function(req, res){
+  Youtube.getInfo({url: videoUrl})
+  .then(video => res.send(video));
+});
+
+app.use('/videos', async (req, res) => {
+  client.get('urlVideos', (err, data) => {
+    if (err) throw err;
+    if (data != null){
+      res.send(data);
     } else {
-      res.send(reply);
+      Youtube.getInfo({url: videoUrl})
+      .then(video => {
+        client.setex('urlVideos', maxTtl, video.formats[2].url);
+        res.send(video.formats[2].url);
+      });      
     }
-  })
+  });
 });
 
 app.use('/', function(req, res){
