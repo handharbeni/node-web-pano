01f648b1b258b00855dfe84029c96dcef4d9bfd6
Muhammad Handharbeni <mhandharbeni@gmail.com>
2020-12-08 03:11:01 +0700

    update

--- express/assets/data.js
+++ express/assets/data.js
@@ -89,7 +89,7 @@ var data = {
         initialEmbed: [
           {
             id: 'wisudaembed',
-            icon: 'https://4t3mkldl.tinifycdn.com/assets/icon/bumper.gif',
+            icon: 'hhttps://4k1spmsj.tinifycdn.com/assets/icon/bumper.gif',
             // https://4t3mkldl.tinifycdn.com/assets/icon/info.png  
             // src: 'http://wisuda2020-prasetiyamulya.com/live-video',
             src: 'https://www.youtube.com/embed/Pi7zMzX-m7Y?autoplay=0&rel=0&showinfo=0&modestbranding=1&enablejsapi=1&controls=1&playsinline=1&origin=http://www.wisuda2020-prasetiyamulya.com',
